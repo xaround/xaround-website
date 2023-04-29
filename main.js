26912f3899a1f1bd535bad408f06abaecdce6fb5
Nik Uzair <nikuzairsc@gmail.com>
2023-04-29 15:29:51 +0800

    Last commit

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ function onScroll() {
 }
 
 function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  var x = document.getElementById("primary-navigation");
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
